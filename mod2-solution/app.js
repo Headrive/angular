cdf2255c787f4d6be15b6ffb806613e32a30d1f7
Headrive <tomheadrive@gmail.com>
2016-09-22 18:53:34 +0200

    mod2-solution

--- mod2-solution/app.js
+++ mod2-solution/app.js
@@ -1,38 +1,97 @@
 (function () {
-        'use strict';
+    'use strict';
 
-        angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController)
-        LunchCheckController.$inject = ['$scope'];
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyShoppingController', ToBuyShoppingController)
+        .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-        function LunchCheckController($scope) {
+    // LIST #1 - ToBuy controller
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
 
-            $scope.lunchMenu = '';
-            $scope.lunchMenuStatus = '';
-            $scope.msgText = '';
+    function ToBuyShoppingController(ShoppingListCheckOffService) {
+        var toBuyList = this;
 
-            $scope.checkIfTooMuch = function () {
+        toBuyList.items = ShoppingListCheckOffService.getItemsToBuy();
 
-                var lunchMenuItems = $scope.lunchMenu.split(",");
-                var nbItems = 0;
-                for (var i = 0; i < lunchMenuItems.length; i++) {
-                    if (lunchMenuItems[i].trim() != "") {
-                        nbItems++;
-                    }
-                }
-                if (nbItems == 0 ) {
-                    $scope.msgText = "Please enter data first !";
-                    $scope.lunchMenuStatus = "has-error";
-                }
-                else if (nbItems > 3 ) {
-                    $scope.msgText = "Too much !";
-                    $scope.lunchMenuStatus = "has-success";
-                } else {
-                    $scope.msgText = "Enjoy !";
-                    $scope.lunchMenuStatus = "has-success";
-                }
-
-
-            };
+        toBuyList.addItem = function (itemIndex) {
+            try {
+                ShoppingListCheckOffService.addItemBought(itemIndex);
+            } catch (error) {
+                toBuyList.errorMessage = error.message;
+            }
         }
+
+    }
+
+
+    // LIST #2 - Bought controller
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+
+    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+        var boughtList = this;
+
+        boughtList.items = ShoppingListCheckOffService.getItemsBought();
+        boughtList.emptyMessage = ShoppingListCheckOffService.getEmptyBoughtMessage();
+    }
+
+    // Service ShoppingListCheckOffService
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        // Lists of shopping items
+        var itemsToBuy = [{
+            name: "Marshmallow",
+            quantity: "100"
+            }, {
+            name: "Milk",
+            quantity: "2"
+            }, {
+            name: "Donuts",
+            quantity: "200"
+            }, {
+            name: "Cookies",
+            quantity: "300"
+            }, {
+            name: "Chocolate",
+            quantity: "5"
+            }];
+        //var itemsToBuy = itemsInitiallyToBuy.slice(0);
+        var itemsBought = [];
+        var emptyBoughtMessage =["Nothing bought yet."];
+       
+       
+        // An item has been Bought -> Move the item selected from ToBuy list to Bought List
+        service.addItemBought = function (itemToBuyIndex) {
+
+            itemsBought.push(itemsToBuy[itemToBuyIndex]);
+            itemsToBuy.splice(itemToBuyIndex, 1);
+            if (emptyBoughtMessage[0]!=""){emptyBoughtMessage[0] ="";}
+            if (itemsToBuy.length == 0) {
+                throw new Error("Everything is bought !");
+            };
+
+        };
+
+
+        // ToBuy List items     
+        service.getItemsToBuy = function () {
+            return itemsToBuy;
+        };
+
+
+        // Bought List items       
+        service.getItemsBought = function () {
+            return itemsBought;
+        };
+
+        service.getEmptyBoughtMessage = function () {
+            return emptyBoughtMessage;
+        };
+
+
+    };
+
+
+
 })();
